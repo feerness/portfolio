7cef46a1ec76000a77692247a4654f4d4baf4e07
ffarina <fernandafarinavaldes@gmail.com>
2025-06-25 20:09:12 -0400

    add new info to home

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://feerness.github.io',
-  base: '/portfolio',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio' : undefined,
 });
